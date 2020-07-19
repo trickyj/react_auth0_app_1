909498fc7ee94d9e189f9fb2a0a983b25fe9a85c
starman <trickyj@MacBook-Pro.local>
2020-07-20 00:03:18 +0530

    working react app, before adding autho0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Nav from "./Nav";
+// import Auth from "./Auth/Auth";
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+// return (
+// <div className="App">
+// {/* <header className="App-header"> */}
+// {/* <img src={logo} className="App-logo" alt="logo" /> */}
+// {/* <p> */}
+// {/* Edit <code>src/App.js</code> and save to reload. */}
+// {/* </p> */}
+// {/* <a */}
+// {/* className="App-link" */}
+// {/* href="https://reactjs.org" */}
+// {/* target="_blank" */}
+// {/* rel="noopener noreferrer" */}
+// {/* > */}
+// {/* Learn React */}
+// </a>
+// {/* </header> */}
+// </div>
+// );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Nav />
+        <div className="body">
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
